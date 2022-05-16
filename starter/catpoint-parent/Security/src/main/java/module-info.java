1faff965834ee9b64e3e0bda4ccd08e47acd6278
Cartoonist <ibemeruchizzy@gmail.com>
2022-05-16 22:44:24 +0100

    compile error fixed

---
+++ starter/catpoint-parent/Security/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module security{
+    requires transitive com.udacity.catpoint.Image;
+    requires transitive java.desktop;
+    requires transitive java.sql;
+    requires miglayout;
+    requires guava;
+    requires gson;
+    requires transitive java.prefs;
+    opens security.service to gson;
+    exports security.service;
+    exports security.data;
+    exports security.application;
+}
